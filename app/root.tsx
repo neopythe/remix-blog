710dad93d744ec17ecad62d136a84acb48f2f460
Evan Bay <neopythe@protonmail.ch>
2022-08-26 07:03:52 -0600

    install Tailwind CSS

--- app/root.tsx
+++ app/root.tsx
@@ -14,22 +14,15 @@ import {
 
 import { ServerStyleContext, ClientStyleContext } from './context'
 
+import styles from './styles/app.css'
+
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 })
 
-export let links: LinksFunction = () => {
-  return [
-    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-    { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-    {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap',
-    },
-  ]
-}
+export let links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 interface DocumentProps {
   children: React.ReactNode
