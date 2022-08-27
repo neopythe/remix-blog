328741fbb14e4857af8d8c535e7479206bfb22c1
Evan Bay <neopythe@protonmail.ch>
2022-08-27 06:33:42 -0600

    add active style to NavLinks

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -12,6 +12,11 @@ export default function Navbar() {
 
   const pages = ['Posts', 'About']
 
+  const activeStyle = {
+    textDecoration: 'underline wavy 2px',
+    textUnderlineOffset: '0.5rem',
+  }
+
   return (
     <nav style={{ backgroundColor: blue[800], color: '#fff' }}>
       <div className="px-8 py-6 flex justify-between items-center max-w-screen-lg mx-auto">
@@ -31,7 +36,11 @@ export default function Navbar() {
           <ul className="hidden gap-8 font-semibold">
             {pages.map((page) => (
               <li key={page}>
-                <NavLink key={page} to={`/${page.toLowerCase()}`}>
+                <NavLink
+                  key={page}
+                  to={`/${page.toLowerCase()}`}
+                  style={({ isActive }) => (isActive ? activeStyle : {})}
+                >
                   {page}
                 </NavLink>
               </li>
