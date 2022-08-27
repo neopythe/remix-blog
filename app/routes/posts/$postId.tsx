83e7cb0e532e114c1e03d17d066c8931f4f48108
Evan Bay <neopythe@protonmail.ch>
2022-08-27 08:46:23 -0600

    display post data on individual post routes

--- app/routes/posts/$postId.tsx
+++ app/routes/posts/$postId.tsx
@@ -1,7 +1,31 @@
-import { useParams } from '@remix-run/react'
+import { useLoaderData } from '@remix-run/react'
+import { Heading } from '@chakra-ui/react'
+import { prisma } from '~/db'
+
+export const loader = async ({ params }: { params: any }) => {
+  const post = await prisma.post.findUnique({
+    where: { id: params.postId },
+  })
+
+  if (!post) throw new Error('Post not found')
+
+  const data = { post }
+  return data
+}
 
 export default function Post() {
-  const params = useParams()
+  const { post } = useLoaderData()
 
-  return <div>Post {params.postId}</div>
+  return (
+    <div className="flex flex-col gap-4">
+      <Heading>{post.title}</Heading>
+      <span>
+        by <span className="text-brand-blue-500">anonymous</span>
+      </span>
+      <span className="text-sm">
+        {new Date(post.createdAt).toLocaleString()}
+      </span>
+      <p>{post.content}</p>
+    </div>
+  )
 }
