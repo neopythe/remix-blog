26c43119216ca19132b0d44c22625d298ef96afb
Evan Bay <neopythe@protonmail.ch>
2022-08-27 08:44:41 -0600

    display post data on post-preview.tsx

--- app/components/post-preview.tsx
+++ app/components/post-preview.tsx
@@ -1,6 +1,19 @@
+import { Link } from '@remix-run/react'
 import { useTheme } from '@chakra-ui/react'
 
-export default function PostPreview({ right }: { right?: boolean }) {
+export default function PostPreview({
+  content,
+  createdAt,
+  id,
+  title,
+  right,
+}: {
+  content: string
+  createdAt: Date
+  id: string
+  right?: boolean
+  title: string
+}) {
   const theme = useTheme()
   const {
     colours: { blue },
@@ -11,18 +24,17 @@ export default function PostPreview({ right }: { right?: boolean }) {
       style={right ? { alignItems: 'flex-end' } : {}}
       className="flex flex-col gap-4"
     >
-      <h2 className="font-semibold">title</h2>
+      <Link to={`posts/${id}`}>
+        <h2 className="font-semibold">{title}</h2>
+      </Link>
       <p style={right ? { textAlign: 'right' } : {}} className="text-sm">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam error,
-        quibusdam doloremque amet temporibus accusantium perspiciatis aut
-        laborum dicta, ratione, culpa quo maiores sunt ab illo modi at.
-        Voluptate, odio?
+        {content}
       </p>
       <div className="flex items-center gap-4">
         <div className="h-8 w-8 rounded-[50%] bg-slate-400"></div>
         <span className="text-xs">
-          Posted 2 days ago by{' '}
-          <span style={{ color: blue[500] }}>billiken</span>
+          Posted <span>{new Date(createdAt).toLocaleString()}</span> by{' '}
+          <span style={{ color: blue[500] }}>anonymous</span>
         </span>
       </div>
     </div>
