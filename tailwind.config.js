e33608cfdec4ab47066a960e00dc562ef60e9b3e
Evan Bay <neopythe@protonmail.ch>
2022-08-27 06:31:59 -0600

    extend Tailwind theme with brand blue colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          blue: {
+            900: '#0D47A1',
+            800: '#1565C0',
+            700: '#1976D2',
+            600: '#1E88E5',
+            500: '#2196F3',
+            400: '#42A5F5',
+            300: '#64B5F6',
+            200: '#90CAF9',
+            100: '#BBDEFB',
+            50: '#E3F2FD',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
