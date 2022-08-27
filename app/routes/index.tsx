3c6875e74b9ec48ec51c877c10b1ac3dd8255670
Evan Bay <neopythe@protonmail.ch>
2022-08-27 08:45:41 -0600

    display recent post previews on landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,19 @@
 import { useTheme } from '@chakra-ui/react'
+import { useLoaderData } from '@remix-run/react'
+import { prisma } from '~/db'
 
 import BoxGrid from '~/components/box-grid'
 import PostPreview from '~/components/post-preview'
+
+export const loader = async () => {
+  const data = {
+    posts: await prisma.post.findMany({
+      take: 3,
+      orderBy: { createdAt: 'desc' },
+    }),
+  }
+  return data
+}
 
 export default function Home() {
   const theme = useTheme()
@@ -9,12 +21,22 @@ export default function Home() {
     colours: { blue },
   } = theme
 
+  const { posts } = useLoaderData()
+
   return (
     <div className="flex flex-col items-end justify-between h-full">
-      <section className="flex flex-col gap-2">
-        <PostPreview />
-        <PostPreview right />
-        <PostPreview />
+      <section className="flex flex-col w-full gap-8">
+        {!posts && <p>Looks like there are no posts. yet. Why not add one?</p>}
+        {posts.map((post: any, index: number) => (
+          <PostPreview
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            content={post.content}
+            createdAt={post.createdAt}
+            right={index % 2 !== 0}
+          />
+        ))}
       </section>
       <BoxGrid colour={blue} />
     </div>
