f94677a0d022d7de8c5bf98656cb7c641fd31354
Evan Bay <neopythe@protonmail.ch>
2022-08-27 08:43:27 -0600

    add functionality for creating new posts

--- app/routes/posts/new.tsx
+++ app/routes/posts/new.tsx
@@ -1,24 +1,21 @@
 import { redirect } from '@remix-run/node'
 import { Heading } from '@chakra-ui/react'
+import { prisma } from '~/db'
 
 import NewPostForm from '~/components/new-post-form'
 
 export const action = async ({ request }: { request: Request }) => {
   const form = await request.formData()
-  const title = form.get('title')
-  const content = form.get('content')
+  const title = String(form.get('title'))
+  const content = String(form.get('content'))
 
   const fields = { title, content }
-  console.log(fields)
+  const post = await prisma.post.create({ data: fields })
 
-  // todo - submit to database
-
-  return redirect('/posts')
+  return redirect(`/posts/${post.id}`)
 }
 
 export const ErrorBoundary = ({ error }: { error: Error }) => {
-  console.log(error)
-
   return (
     <section className="flex flex-col items-center gap-4">
       <Heading>Error</Heading>
@@ -30,7 +27,6 @@ export const ErrorBoundary = ({ error }: { error: Error }) => {
 export default function New() {
   return (
     <section className="flex flex-col items-center gap-4">
-      <Heading>New Post</Heading>
       <NewPostForm />
     </section>
   )
