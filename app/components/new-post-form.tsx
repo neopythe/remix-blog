2e1ae2f54fe962bbcff39b8b3cf9e9c7d7bb35e3
Evan Bay <neopythe@protonmail.ch>
2022-08-27 08:44:02 -0600

    update new-post-form.tsx structure

--- app/components/new-post-form.tsx
+++ app/components/new-post-form.tsx
@@ -1,20 +1,29 @@
 import { Form } from '@remix-run/react'
-import { FormControl, FormLabel, Input, Textarea } from '@chakra-ui/react'
+import {
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Textarea,
+} from '@chakra-ui/react'
 
 import Button from './button'
 
 export default function NewPostForm() {
   return (
-    <Form method="post" className="flex flex-col w-full md:w-2/3 gap-4">
-      <FormControl isRequired>
-        <FormLabel>Title</FormLabel>
-        <Input name="title" type="text" />
-      </FormControl>
-      <FormControl isRequired>
-        <FormLabel>Content</FormLabel>
-        <Textarea name="content" height="24rem" resize="none" />
-      </FormControl>
-      <Button>Submit</Button>
-    </Form>
+    <>
+      <Form method="post" className="flex flex-col w-full md:w-2/3 gap-4">
+        <Heading>New Post</Heading>
+        <FormControl isRequired>
+          <FormLabel>Title</FormLabel>
+          <Input name="title" type="text" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Content</FormLabel>
+          <Textarea name="content" height="24rem" resize="none" />
+        </FormControl>
+        <Button>Submit</Button>
+      </Form>
+    </>
   )
 }
