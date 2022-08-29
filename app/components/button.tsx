7070cb5ca297f25215bddf2722f26a5ce0f84b16
Evan Bay <neopythe@protonmail.ch>
2022-08-29 16:15:30 -0600

    add disabled style for button.tsx

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,14 +1,17 @@
 export default function Button({
   children,
+  isDisabled = false,
   onClick,
 }: {
   children: React.ReactNode
+  isDisabled?: boolean
   onClick?: React.MouseEventHandler<HTMLButtonElement>
 }) {
   return (
     <button
       onClick={onClick}
-      className="px-6 py-1.5 rounded text-white bg-brand-blue-600 hover:bg-brand-blue-400 transition font-semibold"
+      disabled={isDisabled}
+      className="px-6 py-1.5 rounded text-white bg-brand-blue-600 hover:bg-brand-blue-400 transition font-semibold disabled:bg-gray-300"
     >
       {children}
     </button>
