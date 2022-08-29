449572c09e2a438e9003bb8f274cffe1e0abe833
Evan Bay <neopythe@protonmail.ch>
2022-08-29 16:15:04 -0600

    update markup and error handling for $postId.tsx

--- app/routes/posts/$postId.tsx
+++ app/routes/posts/$postId.tsx
@@ -1,5 +1,5 @@
 import { redirect } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
 import { Heading } from '@chakra-ui/react'
 import { prisma } from '~/db'
 
@@ -10,7 +10,10 @@ export const loader = async ({ params }: { params: { postId: string } }) => {
     where: { id: params.postId },
   })
 
-  if (!post) throw new Error('Post not found')
+  if (!post)
+    throw new Response('Not Found', {
+      status: 404,
+    })
 
   const data = { post }
   return data
@@ -36,19 +39,40 @@ export const action = async ({
   }
 }
 
+export function CatchBoundary() {
+  return (
+    <div>
+      <h2>We couldn't find that page!</h2>
+      <br />
+      <Link to="/posts">
+        <Button>Back to posts</Button>
+      </Link>
+    </div>
+  )
+}
+
 export default function Post() {
   const { post } = useLoaderData()
 
   return (
     <div className="flex flex-col gap-4">
       <Heading>{post.title}</Heading>
-      <span>
-        by <span className="text-brand-blue-500">anonymous</span>
-      </span>
-      <span className="text-sm">
-        {new Date(post.createdAt).toLocaleString()}
-      </span>
-      <p>{post.content}</p>
+      <div className="flex flex-col gap-1">
+        <span>
+          by <span className="text-brand-blue-500">anonymous</span>
+        </span>
+        <span className="text-sm">
+          {new Date(post.createdAt).toLocaleString()}
+        </span>
+      </div>
+      <br />
+      <p
+        style={{
+          whiteSpace: 'pre-wrap',
+        }}
+      >
+        {post.content}
+      </p>
       <form method="POST">
         <input type="hidden" name="_method" value="delete" />
         <Button>Delete</Button>
