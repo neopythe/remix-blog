afe03f208fefe0ea3377c6c471e1435ccc875a03
Evan Bay <neopythe@protonmail.ch>
2022-08-29 17:57:22 -0600

    add title length validation on new-post-form.tsx

--- app/components/new-post-form.tsx
+++ app/components/new-post-form.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { Form } from '@remix-run/react'
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
@@ -10,20 +12,35 @@ import {
 import Button from './button'
 
 export default function NewPostForm() {
+  const [input, setInput] = useState('')
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setInput(event.target.value)
+
+  const isError = input.length > 100
+
   return (
-    <>
-      <Form method="post" className="flex flex-col w-full gap-6">
-        <Heading>New Post</Heading>
-        <FormControl isRequired>
-          <FormLabel>Title</FormLabel>
-          <Input name="title" type="text" />
-        </FormControl>
-        <FormControl isRequired>
-          <FormLabel>Content</FormLabel>
-          <Textarea name="content" height="24rem" resize="none" />
-        </FormControl>
-        <Button>Submit</Button>
-      </Form>
-    </>
+    <Form method="post" className="flex flex-col w-full gap-6">
+      <Heading>New Post</Heading>
+      <FormControl isRequired isInvalid={isError}>
+        <FormLabel>Title</FormLabel>
+        <Input
+          name="title"
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+        />
+        {isError && (
+          <FormErrorMessage>
+            Title may not be over 100 characters
+          </FormErrorMessage>
+        )}
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Content</FormLabel>
+        <Textarea name="content" height="20rem" resize="none" />
+      </FormControl>
+      <Button isDisabled={isError}>Submit</Button>
+    </Form>
   )
 }
