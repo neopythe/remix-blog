675246604b44b385b8f35b72931c9ca35fec38b4
Evan Bay <neopythe@protonmail.ch>
2022-08-31 19:59:18 -0600

    set up client-side and server-side validation for posts/new.tsx

--- app/components/new-post-form.tsx
+++ app/components/new-post-form.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Form } from '@remix-run/react'
+import { useState } from 'react';
+import { Form } from '@remix-run/react';
 import {
   FormControl,
   FormErrorMessage,
@@ -7,40 +7,56 @@ import {
   Heading,
   Input,
   Textarea,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
 
-import Button from './button'
+import Button from './button';
 
 export default function NewPostForm() {
-  const [input, setInput] = useState('')
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setInput(event.target.value)
+  const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setContent(event.target.value);
 
-  const isError = input.length > 100
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setTitle(event.target.value);
+
+  const isContentError = content.length < 10;
+  const isTitleError = title.length < 3 || title.length > 100;
 
   return (
     <Form method="post" className="flex flex-col w-full gap-6">
       <Heading>New Post</Heading>
-      <FormControl isRequired isInvalid={isError}>
+      <FormControl isRequired isInvalid={title.length > 0 && isTitleError}>
         <FormLabel>Title</FormLabel>
         <Input
           name="title"
           type="text"
-          value={input}
-          onChange={handleInputChange}
+          value={title}
+          onChange={handleTitleChange}
         />
-        {isError && (
+        {title && isTitleError && (
           <FormErrorMessage>
-            Title may not be over 100 characters
+            Title must be between 3 and 100 characters
           </FormErrorMessage>
         )}
       </FormControl>
-      <FormControl isRequired>
+      <FormControl isRequired isInvalid={content.length > 0 && isContentError}>
         <FormLabel>Content</FormLabel>
-        <Textarea name="content" height="20rem" resize="none" />
+        <Textarea
+          name="content"
+          value={content}
+          onChange={handleContentChange}
+          height="20rem"
+          resize="none"
+        />
+        {content && isContentError && (
+          <FormErrorMessage>
+            Content must be at least 10 characters
+          </FormErrorMessage>
+        )}
       </FormControl>
-      <Button isDisabled={isError}>Submit</Button>
+      <Button isDisabled={isContentError || isTitleError}>Submit</Button>
     </Form>
-  )
+  );
 }
--- app/routes/posts/new.tsx
+++ app/routes/posts/new.tsx
@@ -1,19 +1,44 @@
-import { redirect } from '@remix-run/node'
-import { Heading } from '@chakra-ui/react'
-import { prisma } from '~/db'
+import { json, redirect } from '@remix-run/node';
+import { Heading } from '@chakra-ui/react';
+import { prisma } from '~/db';
 
-import NewPostForm from '~/components/new-post-form'
+import NewPostForm from '~/components/new-post-form';
+
+const validateContent = (content: string) => {
+  if (typeof content !== 'string' || content.length < 10) {
+    return 'Content should be at least 10 characters long';
+  }
+};
+
+const validateTitle = (title: string) => {
+  if (typeof title !== 'string' || title.length < 3) {
+    return 'Title should be at least 3 characters long';
+  }
+  if (title.length > 100) {
+    return 'Title may not be over 100 characters long';
+  }
+};
 
 export const action = async ({ request }: { request: Request }) => {
-  const form = await request.formData()
-  const title = String(form.get('title'))
-  const content = String(form.get('content'))
+  const form = await request.formData();
+  const title = String(form.get('title'));
+  const content = String(form.get('content'));
 
-  const fields = { title, content }
-  const post = await prisma.post.create({ data: fields })
+  const fields = { title, content };
 
-  return redirect(`/posts/${post.id}`)
-}
+  const fieldErrors = {
+    content: validateContent(content),
+    title: validateTitle(title),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return json({ fieldErrors, fields }, { status: 400 });
+  }
+
+  const post = await prisma.post.create({ data: fields });
+
+  return redirect(`/posts/${post.id}`);
+};
 
 export const ErrorBoundary = ({ error }: { error: Error }) => {
   return (
@@ -21,13 +46,13 @@ export const ErrorBoundary = ({ error }: { error: Error }) => {
       <Heading>Error</Heading>
       <pre className="whitespace-pre-wrap">{error.message}</pre>
     </section>
-  )
-}
+  );
+};
 
 export default function New() {
   return (
     <section className="flex flex-col items-center gap-4">
       <NewPostForm />
     </section>
-  )
+  );
 }
