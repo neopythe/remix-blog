3af2642afb7f5cea10e7294be120de954fc5c8dd
Evan Bay <neopythe@protonmail.ch>
2022-09-01 04:38:49 -0600

    query username and include it with loader data on index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,27 +1,42 @@
-import { Link, useLoaderData } from '@remix-run/react'
-import { useTheme } from '@chakra-ui/react'
-import { prisma } from '~/db'
+import { Link, useLoaderData } from '@remix-run/react';
+import { useTheme } from '@chakra-ui/react';
+import { prisma } from '~/db';
 
-import BoxGrid from '~/components/box-grid'
-import PostPreview from '~/components/post-preview'
+import BoxGrid from '~/components/box-grid';
+import PostPreview from '~/components/post-preview';
 
 export const loader = async () => {
+  const posts = await prisma.post.findMany({
+    take: 3,
+    orderBy: { createdAt: 'desc' },
+  });
+
   const data = {
-    posts: await prisma.post.findMany({
-      take: 3,
-      orderBy: { createdAt: 'desc' },
-    }),
-  }
-  return data
-}
+    posts: await Promise.all(
+      posts.map(async (post) => {
+        const user = await prisma.user.findUnique({
+          where: {
+            id: post.userId,
+          },
+        });
+        return {
+          username: user?.username,
+          ...post,
+        };
+      })
+    ),
+  };
+
+  return data;
+};
 
 export default function Home() {
-  const theme = useTheme()
+  const theme = useTheme();
   const {
     colours: { blue },
-  } = theme
+  } = theme;
 
-  const { posts } = useLoaderData()
+  const { posts } = useLoaderData();
 
   return (
     <div className="flex flex-col items-end justify-between h-full">
@@ -38,16 +53,18 @@ export default function Home() {
         {posts.map(
           (
             post: {
-              content: string
-              createdAt: Date
-              id: string
-              title: string
+              content: string;
+              createdAt: Date;
+              id: string;
+              title: string;
+              username: string;
             },
             index: number
           ) => (
             <PostPreview
               key={post.id}
               id={post.id}
+              username={post.username}
               title={post.title}
               content={post.content}
               createdAt={post.createdAt}
@@ -58,5 +75,5 @@ export default function Home() {
       </section>
       <BoxGrid colour={blue} />
     </div>
-  )
+  );
 }
