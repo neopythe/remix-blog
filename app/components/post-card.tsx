76d87dd9332146f186c4b69af82922d5bb4435aa
Evan Bay <neopythe@protonmail.ch>
2022-09-01 04:31:32 -0600

    create post-card.tsx

---
+++ app/components/post-card.tsx
@@ -0,0 +1,26 @@
+import { Link } from '@remix-run/react';
+
+interface Post {
+  content: string;
+  createdAt: Date;
+  id: string;
+  title: string;
+  userId: string;
+  username: string;
+}
+
+export default function PostCard({ post }: { post: Post }) {
+  return (
+    <Link to={`/posts/${post.id}`}>
+      <div className="flex flex-col border rounded border-gray-200 p-4">
+        <span className="font-semibold">{post.title}</span>
+        <span className="flex gap-1">
+          <span>{new Date(post.createdAt).toLocaleString()}</span>
+          <span>
+            by <span className="text-brand-blue-500">{post.username}</span>
+          </span>
+        </span>
+      </div>
+    </Link>
+  );
+}
