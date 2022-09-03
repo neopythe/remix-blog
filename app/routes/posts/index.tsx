481a204a40cdd3d7ca2c94cdadabb751cd35c5b0
Evan Bay <neopythe@protonmail.ch>
2022-09-03 10:40:02 -0600

    display new post button conditionally on posts/index.tsx based on user login status

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -2,30 +2,35 @@ import type { LoaderFunction } from '@remix-run/node';
 
 import { Link, useLoaderData } from '@remix-run/react';
 import { Heading } from '@chakra-ui/react';
+
 import { prisma } from '~/db';
+import { getUser } from '~/utils/session.server';
 
 import Button from '~/components/button';
 import PostCard from '~/components/post-card';
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getUser(request);
   const posts = await prisma.post.findMany({
     orderBy: { createdAt: 'desc' },
     include: { user: { select: { username: true } } },
   });
 
-  return posts;
+  return { posts, user };
 };
 
 export default function Posts() {
-  const posts = useLoaderData();
+  const { posts, user } = useLoaderData();
 
   return (
     <div className="flex flex-col gap-6">
       <div className="flex justify-between items-center">
         <Heading>Posts</Heading>
-        <Link to="/posts/new">
-          <Button>New post</Button>
-        </Link>
+        {user && (
+          <Link to="/posts/new">
+            <Button>New post</Button>
+          </Link>
+        )}
       </div>
       {(!posts || posts.length === 0) && (
         <span>
